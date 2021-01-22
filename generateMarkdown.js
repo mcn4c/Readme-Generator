f3b8618df83e6143e0da489d3744936ffa812c3d
Mary C <mcn4c@virginia.edu>
2021-01-22 11:26:01 -0500

    fixed licelink function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,5 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+
+
 function renderLicenseBadge(license) {
   const splitLicense = license.split(" ").join("");
   if (license != null) {
@@ -12,11 +14,17 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  const splitLicense = license.split(" ").join("");
-  if (splitLicense == null) {
-    return ""
+
+  if (license === "Mozzilla Public Licensing 2.0") {
+    return "https://www.mozilla.org/en-US/MPL/2.0/";
+  } else if (license === "MIT License") {
+    return "https://github.com/git/git-scm.com/blob/master/MIT-LICENSE.txt";
+  } else if (license === "GNU LGPLv3") {
+    return "https://www.gnu.org/licenses/lgpl-3.0.en.html";
+  } else if (license === "Apache 2.0") {
+    return "https://www.apache.org/licenses/LICENSE-2.0"
   } else {
-    return `${splitLicense}`;
+    return "No license";
   }
 }
 
